8abb68b3c25126d31fe3e7326f9674e3a0481b42
TFaucheux <tfaucheux@gmail.com>
2015-03-22 16:43:29 -0400

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,16 @@
-## Below are two functions that cache, and calculate the inverse of a matrix.
+## Below are 2 functions that cache, and calculate the inverse of a matrix.
+
+## Testing
+##   1. Create a 3x3 matrix
+##   mat<-matrix(c(1,4,9,0,-3,2,2,7,8),3,3)
+##   2. Display the output of the matrix
+##   mat
+##   3. Create a Cached version of the Matrix using Function #1  
+##   matInv<-makeCacheMatrix(mat)
+##   4. Use function #2 to return inverse matrix
+##   cacheSolve(matInv)
+##   5. Run a second time to see the "using cached data" message
+##   cacheSolve(matInv)
 
 ## function to create a "matrix" that caches its inverse.
 makeCacheMatrix <- function(m = matrix()) {
@@ -17,6 +29,7 @@ makeCacheMatrix <- function(m = matrix()) {
 cacheSolve <- function(m, ...) {
   inverse <- m$getinv()
   if(!is.null(inverse)) {
+    message("Using cached data...")
     return(inverse)
   }
   data <- m$get()
